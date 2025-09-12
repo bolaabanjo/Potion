646ffbdbc4f4642dc3559708ece94e27c5837081
bolaabanjo <royadebanjo@gmail.com>
2025-09-12 09:59:58 +0100

    Implemented auth

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,9 +3,16 @@
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import Link from "next/link";
 import { ModeToggle } from "@/components/mode-toggle";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+
 
 export const Navbar = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
     const scrolled = useScrollTop();
     
     return (
@@ -15,6 +22,38 @@ export const Navbar = () => {
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner />
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                    <SignInButton mode="modal">
+                        <Button variant="ghost" size="sm">
+                            Login
+                        </Button>
+                    </SignInButton>
+                    <SignInButton mode="modal">
+                        <Button size="sm">
+                            Get Potion Free
+                        </Button>
+                    </SignInButton>
+                    </>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                    <Button variant="ghost" size="sm" asChild>
+                        <Link href="/documents">
+                        Enter Potion                        
+                        </Link>
+                    </Button>
+                    <Button variant="ghost" size="sm">
+                        Settings
+                    </Button>
+                    <UserButton 
+                    afterSignOutUrl="/"
+                    />
+                </>
+                )}
                 <ModeToggle />
             </div>
         </div>
