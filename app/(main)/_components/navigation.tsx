f529f731533e58336e5b92bba7aedd7ec216979a
bolaabanjo <royadebanjo@gmail.com>
2025-09-13 07:28:42 +0100

    Implemented auth

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const Navigation = () => {
+    return ( 
+        <>
+        <aside
+        className="group/sidebar h-full bg-secondary">
+            Navigation!
+        </aside>
+    </>
+    )
+}
