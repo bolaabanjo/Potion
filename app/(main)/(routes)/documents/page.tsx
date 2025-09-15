8cf535d28c68979dd68ddec1b24b1d683cd08749
bolaabanjo <royadebanjo@gmail.com>
2025-09-15 20:54:00 +0100

    Implementing note creation

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -2,12 +2,25 @@
 
 import { Button } from "@/components/ui/button";
 import { useUser } from "@clerk/clerk-react";
+import { useMutation } from "convex/react";
 import { PlusCircle } from "lucide-react";
 import Image from "next/image";
-
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 const DocumentsPage = () => {
     const { user } = useUser();
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating document...",
+            success: "Document created",
+            error: "Error creating document",
+        });
+    }
 
     return (
         <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -28,7 +41,7 @@ const DocumentsPage = () => {
             <h2 className="text-large font-medium">
                 Welcome to {user?.firstName}&apos;s Potion
             </h2>
-            <Button>
+            <Button onClick={onCreate}>
                 <PlusCircle className="h-4 w-4 mr-2" />
                 Create a note
             </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
@@ -41,6 +42,7 @@ export default function RootLayout({
             disableTransitionOnChange
             storageKey="potion-theme"
           >
+            <Toaster position="bottom-center" />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
